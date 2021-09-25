bcac75449404f7294edc8d707b1019958a235b2f
krishnaagupta <krishnaa.gupta@gmail.com>
2021-09-24 18:32:14 -0700

    testcase1

--- TestNG/src/test/java/Pages/LoginPage.java
+++ TestNG/src/test/java/Pages/LoginPage.java
@@ -1,17 +1,73 @@
 package Pages;
 
+import java.io.IOException;
+
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
+
+import com.aventstack.extentreports.Status;
 
 import Base.BasePage;
+import Base.BaseTest;
+import Utils.CommonUtils;
 
 public class LoginPage extends BasePage {
 public LoginPage(WebDriver driver) {
 		super (driver);
 	}
 
-@FindBy(xpath="")
-public WebElement userName;
+@FindBy(xpath="//input[@id='username']")
+//@FindBy(id= "id-identity")
+public  WebElement userName;
+
+@FindBy(id="password")
+public  WebElement password;
+@FindBy(xpath="//*[@id=\"Login\"]")
+public  WebElement login;
+
+@FindBy(xpath="//*[@id=\"rememberUn\"]")
+public  WebElement rememberMe;
+
+@FindBy(xpath="//*[@id=\"error\"]")
+public  WebElement errorMsg;
+
+
+@FindBy(xpath="//*[@id=\"userNav-menuItems\"]/a[5]")
+public  WebElement logOut;
+
+@FindBy(xpath="//*[@id=\"idcard-identity\"]")
+public  WebElement remembermeUserName;
+
+@FindBy(xpath="//*[@id=\"forgot_password_link\"]")
+public  WebElement forgotPassword;
+
+@FindBy(id="un")
+public  WebElement email;
+
+@FindBy(id="continue")
+public  WebElement continueButton;
+
+
+
+public void logintoSFDC(String username1, String password1) throws InterruptedException, IOException {
+	
+	CommonUtils.sendKeys(driver,userName ,5,username1);
+	String usernamegettext=userName.getAttribute("value");
+	Assert.assertEquals(usernamegettext, username1);
+	
+	CommonUtils.sendKeys( driver,password ,5,password1);
+	Assert.assertEquals(password.getAttribute("value"), password1);
+	//System.out.println(usernamegettext+"--"+password.getAttribute("value"));
+	CommonUtils.clickOn(driver,login, 2);
+	String urluu=driver.getTitle();
+	//Assert.assertEquals(driver.getCurrentUrl(), usernamegettext,);
+	System.out.println(urluu);
+
+}
+
 
 }
