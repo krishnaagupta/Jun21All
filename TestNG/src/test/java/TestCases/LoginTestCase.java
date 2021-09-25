43d99f749e98f8a8e858210bd1ca7bb52fc50718
krishnaagupta <krishnaa.gupta@gmail.com>
2021-09-24 19:48:32 -0700

    test

--- TestNG/src/test/java/TestCases/LoginTestCase.java
+++ TestNG/src/test/java/TestCases/LoginTestCase.java
@@ -6,6 +6,7 @@ import javax.swing.text.Utilities;
 
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -14,13 +15,14 @@ import com.sun.tools.sjavac.Log;
 import Base.BaseTest;
 import Pages.LoginPage;
 import Utils.CommonUtils;
-
+// All log in realated test cases testcases  
 public class LoginTestCase extends BaseTest{
 	public static Logger log =Logger.getLogger(LoginTestCase.class);
 
 
 	WebDriver driver;
-	LoginPage lp=new LoginPage(driver);
+	LoginPage lp;
+	
 	CommonUtils common=new CommonUtils();
 
 	
@@ -28,18 +30,23 @@ public class LoginTestCase extends BaseTest{
 	public void beforeTest() throws IOException {
 		driver =getDriver();
 		//test = extent.createTest("Tc1");
-		String url=common.getApplicationproperity("url1");
+		String url=common.getApplicationproperity("url");
 		System.out.println("url--"+ url);
 		driver.get(url);
-		
-
+		lp =new LoginPage(driver);
 	}
 	
+	//hello world
 	
 	@Test
-	public static void tc1login() {
-		//test=extent.createTest("tclogin");
-		System.out.println("hi");
+	public  void tc1login() throws InterruptedException, IOException {
+		String username= common.getApplicationproperity("username");
+		String password= common.getApplicationproperity("password");
+		lp.logintoSFDC(username, password);
+		String usernamegettext=lp.userName.getText();
+		//Assert.assertEquals(username, usernamegettext);
+		
+		System.out.println("user text "+usernamegettext);
 		log.info("test 1");
 		log.debug("hello");
 		//test.info("tc1");
